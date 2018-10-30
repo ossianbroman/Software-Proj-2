3f86da681261e396ee3b95149102227a9a2a824f
Eszter Aszalos <aszalose@gmail.com>
2018-10-30 18:54:41 +0000

    Further bug fixes and optimisation of data by avoiding repetition. Added some comments to explain what each bootstraping function does.

--- R code - covars.R
+++ R code - covars.R
@@ -137,28 +137,28 @@ lmBoot2 <- function(inputData, nBoot) {
 
 system.time(lmBoot2(regData, nBoot = 100000))
 
-# Make the function more efficient 
-
+# Make the function more efficient by using a helper function
 parBadBoot <- function( index, scaleData, N )
 {
+  # sample data with replacement
   bootData <- scaleData[sample( 1:N, N, replace = T ),]
   Xmat <- bootData[,1:2]
   Ymat <- bootData[,3]
   
   # fit the model under this alternative reality
   # Changed the lm part to matrix form
-  
   beta <- solve( t( Xmat ) %*% Xmat ) %*% t( Xmat ) %*% Ymat
   bootResults <- beta
 }
 
-# Final bootstrap function that utilises parLapply function to speed up the process
-
+# Bootstrap function that utilises parLapply function to speed up the process
 bestBadBootBrother <- function( inputData, nBoot )
 {
+  # parallelize
   library(parallel)
   nCores <- detectCores()
   myClust <- makeCluster(nCores-1, type = "FORK")
+  # scale inputData and pass to helper function in form of a matrix
   x <- cbind( 1, scale( inputData$x, scale = F ) )
   y <- scale( inputData$y, scale = F )
   scaleData <- as.matrix( cbind( x, y ) )
@@ -171,7 +171,7 @@ bestBadBootBrother <- function( inputData, nBoot )
                                 scaleData = scaleData, N = N ) 
   
   bootResults <- tempbootResults 
-  
+  # save all bootstap data into matrix and output
   r <- bootResults[[1]]
   c <- bootResults[[2]]
   m <- matrix( c( r, c ), ncol = 2, nrow = N )
@@ -181,10 +181,10 @@ bestBadBootBrother <- function( inputData, nBoot )
 
 system.time( test2 <- bestBadBootBrother( inputData = regData, 100000 ) )
 
-# Extending the above function to accept an arbitraty number of covariates
-
-parBadBootAnyCovars <- function( index, scaleData, N )
+# Extending the above helper function to accept an arbitraty number of covariates
+parBootAnyCovars <- function( index, scaleData, N )
 {
+  # sample data with replacement
   bootData <- scaleData[sample( 1:N, N, replace = T ),]
   Xmat <- bootData[,1:(ncol(bootData)-1)]
   Ymat <- bootData[,ncol(bootData)]
@@ -192,15 +192,17 @@ parBadBootAnyCovars <- function( index, scaleData, N )
   # fit the model under this alternative reality
   # Changed the lm part to matrix form
   beta <- solve( t( Xmat ) %*% Xmat ) %*% t( Xmat ) %*% Ymat
-  bootResults <- beta
+  return(beta)
 }
 
-bestBadBootBrotherAnyCovars1 <- function( nBoot, yDat, ... ) 
+# Include covariates by looping through
+bestBootCovars1 <- function( nBoot, yDat, ... ) 
 {
+  # parallelize
   library(parallel)
   nCores <- detectCores()
   myClust <- makeCluster(nCores-1, type = "FORK")
-  
+  # save x covariates into list and loop through them to constuct covariate matrix
   xDat <- list(...)
   x <- cbind(1, scale(xDat[[1]], scale = F))
   if(length(xDat) > 1) {
@@ -210,35 +212,30 @@ bestBadBootBrotherAnyCovars1 <- function( nBoot, yDat, ... )
   }
   
   y <- scale( yDat, scale = F )
-
+  
+  # unite x covariates and y into matrix
   scaleData <- cbind( x, y )
-
-  bootResults <- array( dim = c( nBoot, 2 ) )
   
   N <- length( yDat ) 
   
-  tempbootResults <- parLapply( myClust, 1:nBoot, parBadBootAnyCovars, 
+  # pass scaled data to helper function to bootstrap
+  tempbootResults <- parLapply( myClust, 1:nBoot, parBootAnyCovars, 
                                 scaleData = scaleData, N = N ) 
-  
-  bootResults <- tempbootResults 
-  
-  r <- bootResults[[1]]
-  c <- bootResults[[2]]
-  m <- matrix( c( r, c ), ncol = length(xDat)+1, nrow = N )
-  
+  # save all bootstap data into matrix and output
+  m <- matrix( unlist(tempbootResults), ncol = length(xDat)+1, nrow = N )  
   return( m ) 
 }
 
-# Changing the for loop on xDat into something faster and more dynamic
-
-bestBadBootBrotherAnyCovars2 <- function( nBoot, yDat, ... ) 
+# Changing the for loop on xDat into something more dynamic
+bestBootCovars2 <- function( nBoot, yDat, ... ) 
 {
+  # parallelize
   library(parallel)
   nCores <- detectCores()
   myClust <- makeCluster(nCores-1, type = "FORK")
-  
+  # save x covariates into list
   xDat <- list(...)
-
+  # create matrix with x covariates and y and scale it
   mdata <- unlist(xDat)
   mcol <- length(xDat)
   mrow <- length( mdata ) / mcol
@@ -249,64 +246,84 @@ bestBadBootBrotherAnyCovars2 <- function( nBoot, yDat, ... )
   mscale <- scale( mnew, scale = F )
   scaleData <- matrix( data = c( baseone, mscale ), nrow = mrow, ncol = mcol + 2 )
   
-  bootResults <- array( dim = c( nBoot, 2 ) )
-  
   N <- length( yDat ) 
-  
-  tempbootResults <- parLapply( myClust, 1:nBoot, parBadBootAnyCovars, 
+  # pass scaled data to helper function to bootstrap
+  tempbootResults <- parLapply( myClust, 1:nBoot, parBootAnyCovars, 
                                 scaleData = scaleData, N = N ) 
-  
-  bootResults <- tempbootResults 
-  
-  r <- bootResults[[1]]
-  c <- bootResults[[2]]
-  m <- matrix( c( r, c ), ncol = length(xDat)+1, nrow = N )
+  # save all bootstap data into matrix and output 
+  m <- matrix( unlist(tempbootResults), ncol = length(xDat)+1, nrow = N )
   
   return( m ) 
 }
 
-bestBadBootBrotherAnyCovars3 <- function( nBoot, yDat, ... ) 
+# Include covariates via matrix
+bestBootCovars3 <- function( nBoot, yDat, ... ) 
 {
+  # parallelize
   library(parallel)
   nCores <- detectCores()
   myClust <- makeCluster(nCores-1, type = "FORK")
   
+  # save x covariates into list, and turn them into a scaled matrix in one step
   xDat <- list(...)
   x <- cbind(1, scale(do.call(cbind, xDat), scale =F ))
   y <- scale( yDat, scale = F )
 
-  scaleData <- cbind( x, y )
-  
-  bootResults <- array( dim = c( nBoot, 2 ) )
+  scaleData <- cbind(x, y)
   
   N <- length( yDat ) 
   
-  tempbootResults <- parLapply( myClust, 1:nBoot, parBadBootAnyCovars, 
+  # pass scaled data to helper function to bootstrap
+  tempbootResults <- parLapply( myClust, 1:nBoot, parBootAnyCovars, 
                                 scaleData = scaleData, N = N ) 
-  
-  bootResults <- tempbootResults 
-  
-  r <- bootResults[[1]]
-  c <- bootResults[[2]]
-  m <- matrix( c( r, c ), ncol = length(xDat)+1, nrow = N )
+  # save all bootstap data into matrix and output
+  m <- matrix( unlist(tempbootResults), ncol = length(xDat)+1, nrow = N )
   
   return( m ) 
 }
 
-## Testing for 1 as well as more x covariate -> version 2 and 3 seems fastest, rerun multiple times, no definite best
-# depends on seed
-system.time( test3 <- bestBadBootBrotherAnyCovars1( 100000, regData$y, regData$x) )
-system.time( test3 <- bestBadBootBrotherAnyCovars2( 100000, regData$y, regData$x) )
-system.time( test3 <- bestBadBootBrotherAnyCovars3( 100000, regData$y, regData$x) )
+# Fastest version by scaling data in one step
+oneBootToRuleThemAll <- function( nBoot, yDat, ... ) 
+{
+  # parallelize
+  library(parallel)
+  nCores <- detectCores()
+  myClust <- makeCluster(nCores-1, type = "FORK")
+  
+  # save x covariates into list
+  xDat <- list(...)
+  # create data matrix in one step by using scale function only once
+  scaleData <- cbind(1, scale(do.call(cbind, xDat, yDat), scale =F ))
 
-system.time( test3 <- bestBadBootBrotherAnyCovars1( 100000, regData$y, regData$x, sampleData$Weight, sampleData$RestPulse, sampleData$RunPulse, sampleData$MaxPulse) )
-system.time( test3 <- bestBadBootBrotherAnyCovars2( 100000, regData$y, regData$x, sampleData$Weight, sampleData$RestPulse, sampleData$RunPulse, sampleData$MaxPulse) )
-system.time( test3 <- bestBadBootBrotherAnyCovars3( 100000, regData$y, regData$x, sampleData$Weight, sampleData$RestPulse, sampleData$RunPulse, sampleData$MaxPulse) )
+  N <- length( yDat ) 
+  
+  # pass scaled data to helper function to bootstrap
+  tempbootResults <- parLapply( myClust, 1:nBoot, parBadBootAnyCovars, 
+                                scaleData = scaleData, N = N ) 
+  # save all bootstap data into matrix and output
+  m <- matrix( unlist(tempbootResults), ncol = length(xDat)+1, nrow = N )
+  
+  return( m ) 
+}
+
+## In all functions above, we return a matrix, where the first column is the intercept
+## and the rest of the columns are best estimate for each covar (in slope terms)
+## To have final bootstrap results, take column means and calculate 95% CI.
+
+## Testing for 1 as well as more x covariates
+system.time( test3 <- bestBootCovars1( 100000, regData$y, regData$x) )
+system.time( test4 <- bestBootCovars2( 100000, regData$y, regData$x) )
+system.time( test5 <- bestBootCovars3( 100000, regData$y, regData$x) )
+system.time( test6 <- oneBootToRuleThemAll( 100000, regData$y, regData$x) )
+
+system.time( test7 <- bestBootCovars1( 100000, regData$y, regData$x, sampleData$Weight, sampleData$RestPulse, sampleData$RunPulse, sampleData$MaxPulse) )
+system.time( test8 <- bestBootCovars2( 100000, regData$y, regData$x, sampleData$Weight, sampleData$RestPulse, sampleData$RunPulse, sampleData$MaxPulse) )
+system.time( test9 <- oneBootToRuleThemAll( 100000, regData$y, regData$x, sampleData$Weight, sampleData$RestPulse, sampleData$RunPulse, sampleData$MaxPulse) )
 
 
 # Compare our efficient bootstrap to initial via microbenchmark
 library(microbenchmark)
-microbenchmark(lmBoot(regData, 100), bestBadBootBrotherAnyCovars3( 100, regData$y, regData$x))
+microbenchmark(lmBoot(regData, 100), oneBootToRuleThemAll( 100, regData$y, regData$x))
 
 
 
